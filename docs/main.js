c53bdd3a9fe0ce183faf7ca405880648f7f9c09c
dominator <dominionoweiagbekeme@gmail.com>
2025-11-12 23:10:54 +0100

    version-4

---
+++ docs/main.js
@@ -0,0 +1,9 @@
+ function openSidebar() {
+   const openBar = document.getElementById('sidebar')
+   openBar.style.display='flex'
+   
+}
+function closeSidebar(){
+    const closeBar = document.getElementById('sidebar')
+    closeBar.style.display='none'
+} 
